4ae81c995050cd474b6bed6eb06ae09ee259870c
Fiona Bianchi <fiona@hiklas.co.uk>
2015-08-20 22:29:53 +0100

    Hit a glitch with loading templates from seperate files, using the script tag doesn't work.
    Adding in code that is a quick and dirty approach to asynchronously loading the templates, ideally need to learn AMD and use Require.js to do things properly.

--- public/javascript/skybill.js
+++ public/javascript/skybill.js
@@ -7,3 +7,40 @@ $(document).ready(function () {
     console.log("Creating compiled sources");
     window.topLevelTemplate = Handlebars.compile(topLevelTemplateSource);
 });
+
+
+function renderTemplate(templateToRender, jsonData, destinationDiv) {
+
+    var resultHtml = templateToRender(jsonData);
+    console.log("Rendered to: " + resultHtml);
+    $('#'+destinationDiv).html(resultHtml);
+}
+
+/**
+ * Copied from a solution on stackoverflow: http://stackoverflow.com/questions/8366733/external-template-in-underscore
+ *
+ * Allows loading of templates from seperate files.
+ *
+ * @param templateName
+ */
+function require_template(templateName) {
+    var template = $('#template_' + templateName);
+    if (template.length === 0) {
+        var tmpl_dir = './templates';
+        var tmpl_url = tmpl_dir + '/' + templateName + '.tmpl';
+        var tmpl_string = '';
+
+        $.ajax({
+            url: tmpl_url,
+            method: 'GET',
+            async: false,
+            contentType: 'text',
+            success: function (data) {
+                tmpl_string = data;
+            }
+        });
+
+        $('head').append('<script id="template_' +
+        templateName + '" type="text/template">' + tmpl_string + '<\/script>');
+    }
+}
