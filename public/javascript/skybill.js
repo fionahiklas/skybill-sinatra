0da4361a7daeb297a2d8cb1c86a1f5209e207a57
Fiona Bianchi <fiona@hiklas.co.uk>
2015-08-21 13:26:22 +0100

    Finally got template fragments working with QUnit.
    Really should migrate to Asynchronous Module Definition and Require.js but will do that later

--- public/javascript/skybill.js
+++ public/javascript/skybill.js
@@ -1,14 +1,44 @@
 /**
  * Needed to load the templates and parse them
  */
-$(document).ready(function () {
+/*$(document).ready(function () {
     console.log("Loading template sources");
     var topLevelTemplateSource = $('#topLevelTemplateHtml').html();
     console.log("Creating compiled sources");
     window.topLevelTemplate = Handlebars.compile(topLevelTemplateSource);
-});
+});*/
 
+/**
+ * Convert a template ID into the name of the script node to hold it
+ *
+ * @param templateID
+ */
+function templateScriptName(templateID) {
+    return 'template_' + templateID;
+}
 
+function templateScriptSearchID(templateID) {
+    return '#' + templateScriptName(templateID);
+}
+
+/**
+ * Find the template source and compile
+ *
+ * @param templateID
+ * @returns {*}
+ */
+function compileTemplate(templateID) {
+    var templateHtml = $(templateScriptSearchID(templateID)).html();
+    return Handlebars.compile(templateHtml);
+}
+
+/**
+ * Render a template to a given node
+ *
+ * @param templateToRender
+ * @param jsonData
+ * @param destinationDiv
+ */
 function renderTemplate(templateToRender, jsonData, destinationDiv) {
 
     var resultHtml = templateToRender(jsonData);
@@ -23,24 +53,30 @@ function renderTemplate(templateToRender, jsonData, destinationDiv) {
  *
  * @param templateName
  */
-function require_template(templateName) {
-    var template = $('#template_' + templateName);
+function require_template(templateID, templatePath) {
+
+    var tmpl_dir = templatePath || '/javascript/templates';
+    console.log('Loading template: ' + templateID + ' from path: ' + templatePath);
+
+    var template = $(templateScriptSearchID(templateID));
     if (template.length === 0) {
-        var tmpl_dir = './templates';
-        var tmpl_url = tmpl_dir + '/' + templateName + '.tmpl';
+        console.log('Couldn\'t find template, loading for: ' + templateID);
+        var tmpl_url = tmpl_dir + '/' + templateID + '.html';
         var tmpl_string = '';
 
+        console.log('Loading data from: ' + tmpl_url);
         $.ajax({
             url: tmpl_url,
             method: 'GET',
             async: false,
-            contentType: 'text',
+            dataType: 'text',
             success: function (data) {
+                console.log('Loaded data for template: ' + tmpl_url);
                 tmpl_string = data;
             }
         });
 
-        $('head').append('<script id="template_' +
-        templateName + '" type="text/template">' + tmpl_string + '<\/script>');
+        console.log('Loaded this template data: ' + tmpl_string + 'length: ' + tmpl_string.length);
+        $('head').append('<script id="' + templateScriptName(templateID) + '" type="text/x-handlebars-template">' + tmpl_string + '<\/script>');
     }
 }
